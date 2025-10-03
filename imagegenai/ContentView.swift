73e6352e410ae52ff761ce3b39d2a7c4dc784b1e
Suwijak Thanawiboon <suwijakza@gmail.com>
2025-10-03 06:18:43 +0200

    Initial Commit

---
+++ imagegenai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  imagegenai
+//
+//  Created by Suwijak Thanawiboon on 3/10/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ imagegenai/imagegenaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  imagegenaiApp.swift
+//  imagegenai
+//
+//  Created by Suwijak Thanawiboon on 3/10/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct imagegenaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
